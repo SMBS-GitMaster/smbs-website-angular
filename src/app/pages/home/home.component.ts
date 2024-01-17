578f62bd9e1d514f0f8ae10c5c95cfabea6bc18a
torres-leo <leojts24@gmail.com>
2024-01-17 13:35:20 -0600

    chore: reorganize models and static data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { HeroPage } from '@interfaces/HeroPage';
 import { RouterModule } from '@angular/router';
 import CardDev from '@components/card-dev/card-dev.component';
 import Hero from '@components/hero/hero.component';
+import { hero } from '@shared/data/constants/homePage';
 
 @Component({
   standalone: true,
@@ -11,12 +11,5 @@ import Hero from '@components/hero/hero.component';
   styleUrls: ['./home.component.scss'],
 })
 export default class HomePage {
-  hero: HeroPage = {
-    title: 'Boost your Software team with LATAM talent.',
-    subtitle: 'Staff augmentation made easy.',
-    description: `Whether you need one developer or an entire team, we have the
-          expertise to find the right fit for your project. Plus, with our
-          flexible engagement models, you can choose the level of support that
-          works best for your needs.`,
-  };
+  public HomeHero = hero;
 }
---
+++ src/app/shared/data/constants/homePage.ts
@@ -0,0 +1,7 @@
+import { HeroPage } from '@models/types/HeroPage';
+
+export const hero: HeroPage = {
+  title: 'Boost your Software team with LATAM talent.',
+  subtitle: 'Staff augmentation made easy.',
+  description: `Whether you need one developer or an entire team, we have the expertise to find the right fit for your project. Plus, with our flexible engagement models, you can choose the level of support that works best for your needs.`,
+};
